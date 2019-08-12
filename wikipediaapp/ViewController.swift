eef5864ee74f804e79a236b318e2e6f14d4d903f
shocho0101 <shocho0101@hotmail.co.jp>
2019-08-12 19:47:50 +0900

    Initial Commit

---
+++ wikipediaapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wikipediaapp
+//
+//  Created by 張翔 on 2019/08/12.
+//  Copyright © 2019 choccho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
